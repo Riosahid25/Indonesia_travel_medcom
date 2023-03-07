b57d0b271e6ed63b409312251bfba054a7b845f0
Riosahid25 <riosahid25@gmail.com>
2023-03-07 14:42:41 +0700

    awal commit semua folder destination

---
+++ medcom_indotravel_danautoba/src/js/main.js
@@ -0,0 +1,21 @@
+// For Scroll-animate star
+// event pada saay link di klik
+$(".page-scroll").on("click", function (e) {
+  // ambil isi atribut href-nya dulu
+  var hrefnya = $(this).attr("href");
+
+  // lalu tankap elemen href yang bersangkutan dari "this"(elemen tertentu)
+  var elementHref = $(hrefnya);
+
+  // lalu pindahkan scro  llnya
+  $("html").animate(
+    {
+      scrollTop: elementHref.offset().top - 50,
+    },
+    1250,
+    "easeInOutExpo"
+  );
+
+  // penghenti atribut href e agar tidak jalan
+  e.preventDefault();
+});
